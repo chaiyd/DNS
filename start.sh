93db494fa90b6e9a93d44a1e2541856c34cd29b6
chaiyd <it016@antpkg.com>
2021-05-18 14:25:52 +0800

    delete aliyun and china CNNIC DNS add ``` 8.8.8.8;    # google DNS 8.8.4.4; 1.1.1.1;    # Cloudflare 1.0.0.1; 4.2.2.2;    # microsoft 4.2.2.1; ```

--- start.sh
+++ start.sh
@@ -2,7 +2,15 @@
 
 set -e
 
-echo "nameserver 223.5.5.5" >> /etc/resolv.conf
+cat <<EOF >/etc/resolv.conf
+nameserver 8.8.8.8    # google DNS
+nameserver 8.8.4.4
+nameserver 1.1.1.1    # Cloudflare
+nameserver 1.0.0.1
+nameserver 4.2.2.2    # microsoft
+nameserver 4.2.2.1
+EOF
+
 sed -i s/baidu.com/$DOMAIN/g /var/bind/pri/domain.zone
 sed -i s/baidu.com/$DOMAIN/g /etc/bind/named.conf
 
